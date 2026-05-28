8e81c107685fd0f838c7a17a988954a5f785ec45
nigamananda-barik18 <bariknigam418@gmail.com>
2026-05-28 15:06:46 +0530

    Create q15.kt

---
+++ q15.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val n = 10
+
+    var first = 0
+    var second = 1
+
+    println("Fibonacci series up to $n terms:")
+
+    for (i in 1..n) {
+        print("$first ")
+
+        val next = first + second
+        first = second
+        second = next
+    }
+}
