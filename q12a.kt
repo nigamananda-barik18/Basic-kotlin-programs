a917193d70ada4ed49597bc252b87a84af75e6bf
nigamananda-barik18 <bariknigam418@gmail.com>
2026-05-28 15:01:17 +0530

    Create q12a.kt

---
+++ q12a.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var a = 10
+    var b = 20
+
+    println("Before swapping: a = $a, b = $b")
+
+    val temp = a
+    a = b
+    b = temp
+
+    println("After swapping: a = $a, b = $b")
+}
